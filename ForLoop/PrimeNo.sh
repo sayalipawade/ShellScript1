cb3f663f3be3a440bc8a90c3c02b07f4c685d097
sayalipawade <sayalipawade05@gmail.com>
2020-03-14 10:42:09 +0530

    Added:Check whether no is prime or not

---
+++ ForLoop/PrimeNo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash +x
+cnt=0;
+read -p "Enter No:" No
+for(( i=1;i<=No;i++))
+do
+		if [ $((No%i)) -eq 0 ]
+		then
+			(( cnt++ ))
+		fi
+done
+		if [ $cnt -eq 2 ]
+		then
+				echo "Prime No"
+		else
+				echo "Not Prime"
+		fi
