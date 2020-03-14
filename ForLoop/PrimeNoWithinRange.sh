a5332446ff8d8b6065e06b63596da5776ef8f6b8
sayalipawade <sayalipawade05@gmail.com>
2020-03-14 10:44:18 +0530

    Added:Printingprime numbers within range

---
+++ ForLoop/PrimeNoWithinRange.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+read -p "Enter start:" start
+read -p "Enter End:" end
+
+for (( i=$start;$i<=$end;i++ ))
+do
+	cnt=0;
+	for(( j=1;$j<=$i;j++))
+	do
+		if [ $(($i%$j)) -eq 0 ]
+		then
+				((cnt++))
+		fi
+	done
+ 	
+	if [ $cnt -eq 2 ]
+	then
+		echo "$i"
+	fi
+done
