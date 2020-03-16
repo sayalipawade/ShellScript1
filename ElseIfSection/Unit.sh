3bdfe966df54174aaf02030e942e806cfb3ae542
sayalipawade <sayalipawade05@gmail.com>
2020-03-16 11:38:49 +0530

    Refactored:Followed the indentations

--- ElseIfSection/Unit.sh
+++ ElseIfSection/Unit.sh
@@ -2,14 +2,14 @@
 read -p "Enter No:" No
 if [ $No -eq 1 ]
 then 
-		echo "Unit"
+	echo "Unit"
 elif [ $No -eq 10 ]
 then
-		echo "Ten"
+	echo "Ten"
 elif [ $No -eq 100 ]
 then 
-		echo "Hundread"
+	echo "Hundread"
 elif [ $No -eq 1000 ]
 then 
-		echo "Thousand"
+	echo "Thousand"
 fi 
