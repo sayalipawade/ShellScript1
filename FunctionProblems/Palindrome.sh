db9600a5bf50735d4ccfdb6a3be50fb3520d6d62
sayalipawade <sayalipawade05@gmail.com>
2020-03-16 09:39:22 +0530

    Added:Check if two numbers are palindrome or not

---
+++ FunctionProblems/Palindrome.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+function IsPalindrome1()
+{
+	No=$1
+	rev=0
+	while [[ $No -ne 0 ]]
+	do
+		rem=$(($No%10))
+		rev=$((($rev*10)+$rem))
+		No=$(($No/10))
+	done
+	if [[ $rev == $1 ]]
+	then
+			echo $rev
+	else
+			echo "Not palindrome"
+	fi
+}
+
+read -p "Enter First Number:" Number1
+read -p "Enter Second Number:" Number2
+result1=$( IsPalindrome1 $Number1 )
+result2=$( IsPalindrome1 $Number2 )
+
+if [ $result1 -eq $result2 ]
+then
+		echo "Both are same and Palindrome"
+else
+		echo "Both are not same"
+fi
+		
+	
