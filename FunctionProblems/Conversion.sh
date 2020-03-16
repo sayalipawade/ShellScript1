d3fb8b6488891e965ff4c71b8dbf53138ee6075c
sayalipawade <sayalipawade05@gmail.com>
2020-03-16 09:37:15 +0530

    Added:Performing the conversions degF to degC and degC to degF

---
+++ FunctionProblems/Conversion.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x 
+function Fahrenheit()
+{
+	read -p "Enter Temperature:" temperature
+	degF=`echo "$(($temperature*(9/5)+32))" | bc -l`
+	echo $degF
+}
+function Celcius()
+{
+	read -p "Enter Temprature:" temperature1
+	degC=`echo "$((($temperature1-32)*5/9))" | bc -l`
+	echo $degC
+}
+
+echo "1.Celcius to Fahrenheit conversion"
+echo "2.Fahrenheit to Celcius Conversion" 
+read -p "Enter your choice:" choice
+case $choice in
+		1) Fahrenheit  
+			;;
+		2) Celcius 
+			;;
+		*)
+			echo "Wrong Input"
+esac
+
+			
+ 
