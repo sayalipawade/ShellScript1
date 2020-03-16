527ceeb3ae76d3601db762ea75b1e6cce639d75d
sayalipawade <sayalipawade05@gmail.com>
2020-03-16 10:34:02 +0530

    Refactor:followed proper identations

--- WhileLoop/Gambler.sh
+++ WhileLoop/Gambler.sh
@@ -5,26 +5,26 @@ cnt=0;
 i=0;
 while [ true ]
 do
-		bet=$((RANDOM%2));
-		if [ $bet -eq 0 ]
-		then
-			stake=$(($stake-1));
-			((i++))
-		else
-			stake=$(($stake+$bet))
-			((cnt++))
-			((i++))
-		fi
+	bet=$((RANDOM%2));
+	if [ $bet -eq 0 ]
+	then
+		stake=$(($stake-1));
+		((i++))
+	else
+		stake=$(($stake+$bet))
+		((cnt++))
+		((i++))
+	fi
 
-		if [ $stake -le 0 ]
-		then
-				echo "Gambler lost!!"
-				break;
-		elif [ $goal -lt $stake ]
-		then
-				echo "Gambler won!!! Gambler won $cnt times"
-				break;
-		fi
+	if [ $stake -le 0 ]
+	then
+		echo "Gambler lost!!"
+		break
+	elif [ $goal -lt $stake ]
+	then
+		echo "Gambler won!!! Gambler won $cnt times"
+		break
+	fi
 done
 
 			
