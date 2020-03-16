c4ba6bbb88df03b10ccf8057e32e75d5fc43c1ba
sayalipawade <sayalipawade05@gmail.com>
2020-03-16 10:52:41 +0530

    Refactored:Followed proper indentations

--- ForLoop/PrimeNo.sh
+++ ForLoop/PrimeNo.sh
@@ -3,10 +3,10 @@ cnt=0;
 read -p "Enter No:" No
 for(( i=1;i<=No;i++))
 do
-		if [ $((No%i)) -eq 0 ]
-		then
-			(( cnt++ ))
-		fi
+	if [ $((No%i)) -eq 0 ]
+	then
+		(( cnt++ ))
+	fi
 done
 if [ $cnt -eq 2 ]
 then
